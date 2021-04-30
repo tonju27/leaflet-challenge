3a31f49cec18920597e26a2180d3cd336dd7f73c
Anthony Njuguna <tonju2@yahoo.com>
2021-04-30 16:19:30 -0500

    2nd version queryUrl and get request attempt 1

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,3 @@
+console.log("config.js loaded")
+// API key
+const API_KEY = "pk.eyJ1IjoidG9uanUyIiwiYSI6ImNrbnQ2YmtjMDAxa3IydXFvaHc0YmtoYzIifQ.q3Lo4OkcN-tSfzP4p2aXsA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,12 @@
+console.log("logic.js loaded")
+
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+  });
+
+  
